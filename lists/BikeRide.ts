ca9b7127b559bd748903eddf52414f5972cf2fe2
Ben Higginbotham <bicycleben88@gmail.com>
2021-09-08 00:29:07 -0400

    add access for logged in user to create a bike ride

--- lists/BikeRide.ts
+++ lists/BikeRide.ts
@@ -1,13 +1,25 @@
 import { integer, relationship, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 
+const isSignedIn = ({ session }) => !!session;
+
 export const BikeRide = list({
-  // access:
+  access: {
+    create: isSignedIn,
+    read: isSignedIn,
+    update: isSignedIn,
+    delete: isSignedIn,
+  },
   fields: {
     date: text({ isRequired: true }),
     miles: integer({ isRequired: true }),
     hours: integer(),
     minutes: integer(),
-    user: relationship({ ref: "User.bikeRide" }),
+    user: relationship({
+      ref: "User.bikeRide",
+      defaultValue: ({ context }) => ({
+        connect: { id: context.session.itemId },
+      }),
+    }),
   },
 });
